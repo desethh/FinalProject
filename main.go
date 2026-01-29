e35f9ae8cd598d004def6155387f767a46593c89
desethh <some@gmail.com>
2026-01-29 15:02:49 +0500

    Update

--- main.go
+++ main.go
@@ -3,6 +3,9 @@ package main
 import (
 	"html/template"
 	"net/http"
+
+	"github.com/google/uuid"
+	"github.com/gorilla/websocket"
 )
 
 type PageData struct {
@@ -13,6 +16,30 @@ type PageData struct {
 type DB struct {
 	username string
 	password string
+}
+
+type Client struct {
+	Username string
+	Conn     *websocket.Conn
+}
+
+type Room struct {
+	ID       string
+	Owner    string
+	Clients  map[*Client]bool
+	Messages []Message
+}
+
+type Message struct {
+	User string `json:"user"`
+	Text string `json:"text"`
+}
+
+var rooms = make(map[string]*Room)
+var upgrader = websocket.Upgrader{
+	CheckOrigin: func(r *http.Request) bool {
+		return true
+	},
 }
 
 func main() {
@@ -54,6 +81,69 @@ func main() {
 				return
 			}
 			http.Redirect(w, r, "/page", http.StatusSeeOther)
+		}
+	})
+
+	http.HandleFunc("/create-room", func(w http.ResponseWriter, r *http.Request) {
+		username := r.Header.Get("X-Username")
+		if username == "" {
+			http.Error(w, "Unauthorized", http.StatusUnauthorized)
+			return
+		}
+
+		roomID := uuid.New().String()
+
+		rooms[roomID] = &Room{
+			ID:      roomID,
+			Owner:   username,
+			Clients: make(map[*Client]bool),
+		}
+
+		w.Write([]byte(roomID))
+	})
+	http.HandleFunc("/ws", func(w http.ResponseWriter, r *http.Request) {
+		username := r.Header.Get("X-Username")
+		roomID := r.URL.Query().Get("room")
+
+		room, ok := rooms[roomID]
+		if !ok {
+			http.Error(w, "Room not found", http.StatusNotFound)
+			return
+		}
+
+		conn, err := upgrader.Upgrade(w, r, nil)
+		if err != nil {
+			return
+		}
+
+		client := &Client{
+			Username: username,
+			Conn:     conn,
+		}
+
+		room.Clients[client] = true
+		for _, msg := range room.Messages {
+			client.Conn.WriteJSON(msg)
+		}
+
+		for {
+			_, msg, err := conn.ReadMessage()
+			if err != nil {
+				delete(room.Clients, client)
+				conn.Close()
+				break
+			}
+
+			message := Message{
+				User: username,
+				Text: string(msg),
+			}
+			room.Messages = append(room.Messages, message)
+
+			for c := range room.Clients {
+				c.Conn.WriteJSON(message)
+			}
+
 		}
 	})
 
