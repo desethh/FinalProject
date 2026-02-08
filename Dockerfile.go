bc7d7296b2faf55fd6a9caedd7010dc737ecac3e
desethh <some@gmail.com>
2026-02-09 03:26:51 +0500

    Docker fix1

--- Dockerfile.go
+++ Dockerfile.go
@@ -1,14 +1,18 @@
 FROM golang:1.22-alpine AS builder
 WORKDIR /app
 
-COPY go.mod go.sum ./
-RUN go mod download
+RUN apk add --no-cache git ca-certificates
 
 COPY . .
+
+ENV GOPROXY=https://proxy.golang.org,direct
+
+RUN go mod download
 
 RUN CGO_ENABLED=0 GOOS=linux GOARCH=amd64 go build -o server ./main.go
 
 FROM alpine:3.20
+RUN apk add --no-cache ca-certificates
 WORKDIR /app
 COPY --from=builder /app/server ./server
 
